51fcbee8c3142d82559c43eaaed7fa6509c249c4
Carlos Wilson <carlos.wilson@definityfirst.com>
2015-08-24 15:43:12 -0500

    Se agrega enlaces a cada Actividad de los Retos

--- Scripts/app/programa/controllers/dashboard.etapa.js
+++ Scripts/app/programa/controllers/dashboard.etapa.js
@@ -36,6 +36,11 @@ angular
             
             $scope.challengeName = "MIS RETOS";
             
+            $scope.goToUrl = function(url) {
+                $location.path(url);
+                
+            }
+            
             $scope.logroEducativo = {
                 "userId" : 53,
                 
@@ -45,12 +50,12 @@ angular
                             
                 //El key "status" indica si la etapa ya se completó (1) o no se ha completado (9)
                 
-                "retosPorEtapa" :  [    { "idEtapa" : 1, "retos" : [    {"idReto": 1, "name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion inicial", "status" : 1} ] },
-                                                                        {"idReto": 2, "name" : "Cuarto de recursos", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 1} ] },
-                                                                        {"idReto": 3, "name" : "Conócete", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 0},  {"id" : 2, "name" : "Reto múltiple", "status" : 0},  {"id" : 3, "name" : "Punto de encuentro", "status" : 0},  {"id" : 4, "name" : "Zona de contacto", "status" : 0} ]},
-                                                                        {"idReto": 4, "name" : "Mis sueños", "icon" : "assets/images/img-rotator-01-lg.png",  "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 0},  {"id" : 2, "name" : "Mis gustos", "status" : 0},  {"id" : 3, "name" : "Mis cualidades", "status" : 0},  {"id" : 4, "name" : "Sueña", "status" : 0},  {"id" : 5, "name" : "Punto de encuentro", "status" : 0} ] },
-                                                                        {"idReto": 5, "name" : "Cabina de soporte", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Chat", "status" : 0} ]},
-                                                                        {"idReto": 6, "name" : "Exploración final", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion final", "status" : 0} ]  }
+                "retosPorEtapa" :  [    { "idEtapa" : 1, "retos" : [    {"idReto": 1, "name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion inicial", "link": "/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial", "status" : 1} ] },
+                                                                        {"idReto": 2, "name" : "Cuarto de recursos", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "link" : "/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia",  "status" : 1} ] },
+                                                                        {"idReto": 3, "name" : "Conócete", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "link":"/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia", "status" : 0},  {"id" : 2, "name" : "Reto múltiple", "link":"/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple",  "status" : 0},  {"id" : 3, "name" : "Punto de encuentro", "link":"/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/zv_puntodeencuentro", "status" : 0},  {"id" : 4, "name" : "Zona de contacto", "link":"/ZonaDeVuelo/Conocete/ZonaDeContacto','Zona de contacto", "status" : 0} ]},
+                                                                        {"idReto": 4, "name" : "Mis sueños", "icon" : "assets/images/img-rotator-01-lg.png",  "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "link":"/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia", "status" : 0},  {"id" : 2, "name" : "Mis gustos", "link":"/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos", "status" : 0},  {"id" : 3, "name" : "Mis cualidades", "link":"/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades", "status" : 0},  {"id" : 4, "name" : "Sueña", "link": "/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena", "status" : 0},  {"id" : 5, "name" : "Punto de encuentro", "link":"/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro", "status" : 0} ] },
+                                                                        {"idReto": 5, "name" : "Cabina de soporte", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Chat", "link":"/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat", "status" : 0} ]},
+                                                                        {"idReto": 6, "name" : "Exploración final", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion final", "link":"/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal", "status" : 0} ]  }
                                                                     ]},
                                                                     
                                         { "idEtapa" : 2, "retos" : [    {"idReto": 7, "name" : "Reto 1", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Actividad X", "status" : 0} ] },
@@ -134,9 +139,8 @@ angular
                     avanceEnEtapaActual += $scope.logroEducativo.retosPorEtapa[etapaActual].retos[j].actividades[k].status;  
                     totalActividadesEnEtapaActual++;             
                 }            
-              }                      
-              
-            //alert(avanceEnEtapaActual + " " + totalActividadesEnEtapaActual);
+              }       
+           
             
             //$scope.avanceGlobal = Math.ceil(avanceGlobal*100/totalDeActividades);
             $scope.avanceEnEtapaActual = Math.ceil(avanceEnEtapaActual*100/totalActividadesEnEtapaActual);
