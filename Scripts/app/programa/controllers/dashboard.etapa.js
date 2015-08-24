c967702821abf2a7ef3fe69077dfe49af0774b3b
Carlos Wilson <carlos.wilson@definityfirst.com>
2015-08-24 15:11:44 -0500

    Cálculo de avances parcial y global del usuario

--- Scripts/app/programa/controllers/dashboard.etapa.js
+++ Scripts/app/programa/controllers/dashboard.etapa.js
@@ -23,17 +23,47 @@ angular
             $scope.$emit('HidePreloader'); //hide preloader
             
             //
+            /*
+            var stage = JSON.parse(localStorage.getItem('stage'));
+            var userCourse = JSON.parse(localStorage.getItem('usercourse'));
+            
+            var id = stage.id;
+            
+            var etapa1 = userCourse.stages[0];
+            */
+            //var challenges = etapa1.
+            
             
             $scope.challengeName = "MIS RETOS";
             
             $scope.logroEducativo = {
                 "userId" : 53,
-                "etapas" : [{"etapa1" : {"name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "status" : 1}}, 
-                            {"etapa2" : {"name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "status" : 1}}, 
-                            {"etapa3" : {"name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "status" : 1}}
-                           ],
-                "etapasLogradas" : [1],  //Etapas completadas
-                "retos" : [ {"name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", 
+                
+                "etapas" : [{"idEtapa" : 0, "name" : "Zona de Vuelo", "status" : 0}, 
+                            {"idEtapa" : 1, "name" : "Zona de Navegación", "status" : 0}, 
+                            {"idEtapa" : 2, "name" : "Zona de Aterrizaje", "status" : 0}  ],
+                            
+                //El key "status" indica si la etapa ya se completó (1) o no se ha completado (9)
+                
+                "retosPorEtapa" :  [    { "idEtapa" : 1, "retos" : [    {"idReto": 1, "name" : "Exploración inicial", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion inicial", "status" : 1} ] },
+                                                                        {"idReto": 2, "name" : "Cuarto de recursos", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 1} ] },
+                                                                        {"idReto": 3, "name" : "Conócete", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 0},  {"id" : 2, "name" : "Reto múltiple", "status" : 0},  {"id" : 3, "name" : "Punto de encuentro", "status" : 0},  {"id" : 4, "name" : "Zona de contacto", "status" : 0} ]},
+                                                                        {"idReto": 4, "name" : "Mis sueños", "icon" : "assets/images/img-rotator-01-lg.png",  "actividades" : [ {"id" : 1, "name" : "Fuente de energía", "status" : 0},  {"id" : 2, "name" : "Mis gustos", "status" : 0},  {"id" : 3, "name" : "Mis cualidades", "status" : 0},  {"id" : 4, "name" : "Sueña", "status" : 0},  {"id" : 5, "name" : "Punto de encuentro", "status" : 0} ] },
+                                                                        {"idReto": 5, "name" : "Cabina de soporte", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Chat", "status" : 0} ]},
+                                                                        {"idReto": 6, "name" : "Exploración final", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Exploracion final", "status" : 0} ]  }
+                                                                    ]},
+                                                                    
+                                        { "idEtapa" : 2, "retos" : [    {"idReto": 7, "name" : "Reto 1", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Actividad X", "status" : 0} ] },
+                                                                        {"idReto": 8, "name" : "Reto 2", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Actividad X", "status" : 0} ]},                                                                    
+                                                                    ]},
+                                                                    
+                                        { "idEtapa" : 3, "retos" : [    {"idReto": 9, "name" : "Reto 1", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Actividad X", "status" : 0} ] },
+                                                                        {"idReto": 10, "name" : "Reto 1", "icon" : "assets/images/img-rotator-01-lg.png", "actividades" : [ {"id" : 1, "name" : "Actividad X", "status" : 0} ] },                                                                    
+                                                                    ]}
+                                    ]
+            };
+            
+            /*                           
                                 "actividades" : [{"name" : "Exploracion inicial", "status" : 0}]}, 
                             {"name" : "Cuarto de recursos", "icon" : "assets/images/img-rotator-01-lg.png", 
                                "actividades" : [{"name" : "Fuente de energia", "status" : 1}]}, 
@@ -46,25 +76,70 @@ angular
                             {"name" : "Exploración final", "icon" : "assets/images/img-rotator-01-lg.png", 
                                 "actividades" : [{"name" : "Exploracion final", "status" : 1}]}, 
                           ]                            
-                };
+                },
+                */
                 
-              var puntosObtenidos = 0;
+              var avanceGlobal = 0;  //Avance global del usuario, considerando todas las Actividdes de todos los Retos de todas las Etapas
+              var avanceEtapa = [0, 0, 0]; //Avance del usuario, considerando todas las Actividades de todos los Retos, por cada Etapa
               
-              var numRetos = $scope.logroEducativo.retos.length;
+              var totalDeEtapas = $scope.logroEducativo.etapas.length; //Número total de Etapas
+              var totalDeRetos = 0;   //Número total de Retos, considetando todos los Retos de todas las Etapas
+              var totalDeActividades = 0; //Número total de Actividdes, considerando todos los Retos de todas las Etapas
               
-              for (var i = 0; i < numRetos; i++) {
-                  var numActividades = $scope.logroEducativo.retos[i].actividades.length;
-                  
-                  for (var j = 0; j < numActividades; j++) {
-                      puntosObtenidos = puntosObtenidos + $scope.logroEducativo.retos[i].actividades[j].status;
-                  }
-                  
+              //Obtención del total de Retos
+              for (var i = 0; i < totalDeEtapas; i++) {
+                totalDeRetos += $scope.logroEducativo.retosPorEtapa[i].retos.length;           
               }
               
-              $scope.puntosObtenidos = Math.ceil(puntosObtenidos*100/13);
+              //Total del total de Actividades en todas las Etapas; cada Actividad vale 0 o 1 punto.
+              for (var i = 0; i < totalDeEtapas; i++) {
+                  
+                  var retos = $scope.logroEducativo.retosPorEtapa[i].retos.length;
+                  
+                  for (var j = 0; j < retos; j++) {
+                        totalDeActividades += $scope.logroEducativo.retosPorEtapa[i].retos[j].actividades.length;               
+                }                      
+              }
               
+              var idEtapa = 0;              
+              
+              //var numRetos = $scope.logroEducativo.retosPorEtapa[idEtapa].retos.length;  //Total de retos en la Etapa 1 (índice 0)
+              
+              
+              //Cálculo del avance global del usuario.
+              for (var i = 0; i < totalDeEtapas; i++) {
+                  
+                  var retosEnEtapa = $scope.logroEducativo.retosPorEtapa[i].retos.length;
+                  
+                  for (var j = 0; j < retosEnEtapa; j++) {
+                        var actividadesEnReto = $scope.logroEducativo.retosPorEtapa[i].retos[j].actividades.length;     
+                        
+                    for (var k = 0; k < actividadesEnReto; k++) {
+                        avanceGlobal += $scope.logroEducativo.retosPorEtapa[i].retos[j].actividades[k].status;               
+                    }            
+                 }                      
+              }
+              
+              //Cálculo del avance global del usuario en la Etapa actual
+              var etapaActual = 0;   //0, 1, 2
+              var avanceEnEtapaActual = 0;
+              var totalActividadesEnEtapaActual = 0;
+              
+              var retosEnEtapaActual = $scope.logroEducativo.retosPorEtapa[etapaActual].retos.length;
+              
+              for (var j = 0; j < retosEnEtapaActual; j++) {
+                var numActividadesParcial = $scope.logroEducativo.retosPorEtapa[etapaActual].retos[j].actividades.length;     
+                        
+                for (var k = 0; k < numActividadesParcial; k++) {
+                    avanceEnEtapaActual += $scope.logroEducativo.retosPorEtapa[etapaActual].retos[j].actividades[k].status;  
+                    totalActividadesEnEtapaActual++;             
+                }            
+              }                      
+              
+            //alert(avanceEnEtapaActual + " " + totalActividadesEnEtapaActual);
             
-            //$scope.puntosObtenidos = 92.3;
+            //$scope.avanceGlobal = Math.ceil(avanceGlobal*100/totalDeActividades);
+            $scope.avanceEnEtapaActual = Math.ceil(avanceEnEtapaActual*100/totalActividadesEnEtapaActual);
             
             $scope.playVideo = function(videoAddress, videoName){                 
                  //var videoAddress = "assets/media";
