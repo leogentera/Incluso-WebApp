ebf841783fbc1fd1c41873f0b469e1b9038ecda5
Carlos Wilson <carlos.wilson@definityfirst.com>
2015-08-27 09:48:43 -0500

    Improved dashboard.etapa.js

--- Scripts/app/programa/controllers/dashboard.etapa.js
+++ Scripts/app/programa/controllers/dashboard.etapa.js
@@ -1,5 +1,3 @@
-
-
 angular
     .module('incluso.programa.dashboard.etapa', [])
     .controller('programaEtapaController', [
@@ -7,16 +5,16 @@ angular
         '$scope',
         '$location',
         '$routeParams',
-		'$timeout',
-		'$rootScope',
-		'$http',
+        '$timeout',
+        '$rootScope',
+        '$http',
         '$modal',
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $modal) {
             /* $routeParams.stageId */
             _httpFactory = $http;
             $scope.Math = window.Math;
             $scope.$emit('ShowPreloader'); //show preloader
-            
+
             //$rootScope.pageName = "EstaciÃ³n: ConÃ³cete";
             $rootScope.navbarBlue = true;
             $rootScope.showToolbar = true;
@@ -28,30 +26,30 @@ angular
 //
             //_createNotification();
 
-        
+
             $scope.activitiesCompletedInCurrentStage = [];
 
             $scope.isCollapsed = false;
 
             /*
-            $scope.theActivities = [    [{"name" : "Exploracion Inicial", "status" : 0, "link" : "/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"}],
-                                        [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"}],
-                                        [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia"}, {"name" : "Reto múltiple", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple"}, {"name" : "Punto de encuentro", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64"}, {"name" : "Zona de contacto", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/ZonaDeContacto"}],
-                                        [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia"}, {"name" : "Mis cualidades", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades"}, {"name" : "Mis gustos", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos"}, {"name" : "Sueña", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena"}, {"name" : "Punto de encuentro", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"}],
-                                        [{"name" : "Cabina de soporte", "status" : 0, "link":"/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"}],
-                                        [{"name" : "Exploración final", "status" : 0, "link":"/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"}]  ];
-            */
+             $scope.theActivities = [    [{"name" : "Exploracion Inicial", "status" : 0, "link" : "/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"}],
+             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"}],
+             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia"}, {"name" : "Reto múltiple", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple"}, {"name" : "Punto de encuentro", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64"}, {"name" : "Zona de contacto", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/ZonaDeContacto"}],
+             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia"}, {"name" : "Mis cualidades", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades"}, {"name" : "Mis gustos", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos"}, {"name" : "Sueña", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena"}, {"name" : "Punto de encuentro", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"}],
+             [{"name" : "Cabina de soporte", "status" : 0, "link":"/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"}],
+             [{"name" : "Exploración final", "status" : 0, "link":"/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"}]  ];
+             */
 
 
             var activitiesURLs = [
-                                    ["/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"],
-                                    ["/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"],
-                                    [ "/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia", "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple", "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64", "/ZonaDeVuelo/Conocete/ZonaDeContacto"],
-                                    [ "/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia", "/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos", "/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades", "/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena", "/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"],
-                                    ["/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"],
-                                    ["/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"]];
+                ["/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"],
+                ["/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"],
+                ["/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia", "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple", "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64", "/ZonaDeVuelo/Conocete/ZonaDeContacto"],
+                ["/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia", "/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos", "/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades", "/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena", "/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"],
+                ["/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"],
+                ["/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"]];
 
-            $scope.goToUrl = function(challenge, activity) {
+            $scope.goToUrl = function (challenge, activity) {
                 $location.path(activitiesURLs[challenge][activity]);
             };
 
@@ -97,43 +95,40 @@ angular
                 }
             }
 
-            $scope.avanceEnEtapaActual = Math.ceil(avanceEnEtapaActual*100/totalActividadesEnEtapaActual);
+            $scope.avanceEnEtapaActual = Math.ceil(avanceEnEtapaActual * 100 / totalActividadesEnEtapaActual);
 
 
             $scope.retosIconos = {
-                "Exploración Inicial" : "assets/images/challenges/stage-1/img-evaluacion inicial.svg",
-                "Cuarto de recursos" : "assets/images/challenges/stage-1/img-cuarto-recursos.svg",
-                "Conócete" : "assets/images/challenges/stage-1/img-conocete.svg",
-                "Mis sueños" : "assets/images/challenges/stage-1/img-mis-suenos.svg",
-                "Cabina de soporte" : "assets/images/challenges/stage-1/img-cabina-soporte.svg",
-                "Exploración final" : "assets/images/challenges/stage-1/img-evaluacion final.svg"
+                "Exploración Inicial": "assets/images/challenges/stage-1/img-evaluacion inicial.svg",
+                "Cuarto de recursos": "assets/images/challenges/stage-1/img-cuarto-recursos.svg",
+                "Conócete": "assets/images/challenges/stage-1/img-conocete.svg",
+                "Mis sueños": "assets/images/challenges/stage-1/img-mis-suenos.svg",
+                "Cabina de soporte": "assets/images/challenges/stage-1/img-cabina-soporte.svg",
+                "Exploración final": "assets/images/challenges/stage-1/img-evaluacion final.svg"
             };
 
 
-
-
-            
-            $scope.playVideo = function(videoAddress, videoName){
-                 playVideo(videoAddress, videoName);
+            $scope.playVideo = function (videoAddress, videoName) {
+                playVideo(videoAddress, videoName);
             };
 
             $scope.openClosingStageModal = function (size) {
                 console.log("opening");
                 //setTimeout(function(){ 
-                    var modalInstance = $modal.open({
-                        animation: $scope.animationsEnabled,
-                        templateUrl: 'ClosingStage.html',
-                        controller: 'closingStageController',
-                        size: size,
-                        windowClass: 'closing-stage-modal user-help-modal'
-                    });
-                    console.log("modal open closing");
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'ClosingStage.html',
+                    controller: 'closingStageController',
+                    size: size,
+                    windowClass: 'closing-stage-modal user-help-modal'
+                });
+                console.log("modal open closing");
                 //}, 1000);
             };
 
         }])
-        .controller('closingStageController', function ($scope, $modalInstance) {
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        });
+    .controller('closingStageController', function ($scope, $modalInstance) {
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
