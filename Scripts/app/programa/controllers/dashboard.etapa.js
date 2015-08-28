03d5dfd6eaad9c5380da9c21558bdf6038396a77
Nestor Madrigal <madrigalnestor@gmail.com>
2015-08-27 20:10:08 -0500

    [merge]

--- Scripts/app/programa/controllers/dashboard.etapa.js
+++ Scripts/app/programa/controllers/dashboard.etapa.js
@@ -45,22 +45,34 @@ angular
             $scope.scrollToTop();
             $scope.$emit('HidePreloader'); //hide preloader
 
-            successChallengeCallback();
-            //_createNotification();
+            $scope.openModal = function (size) {
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'OpeningStageModal.html',
+                    controller: 'OpeningStageController',
+                    size: size,
+                    windowClass: 'user-help-modal'
+                });
+                console.log("modal open");
+            };
 
+            $scope.openModal();
 
+            var closingStageModal = localStorage.getItem('closeStageModal');
+            if (closingStageModal == 'true') {
+                openStageModal();
+                localStorage.setItem('closeStageModal', 'false');
+            }        
+            
             $scope.activitiesCompletedInCurrentStage = [];
-
             $scope.isCollapsed = false;
 
-            /*
-             $scope.theActivities = [    [{"name" : "Exploracion Inicial", "status" : 0, "link" : "/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"}],
-             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"}],
-             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia"}, {"name" : "Reto múltiple", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple"}, {"name" : "Punto de encuentro", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64"}, {"name" : "Zona de contacto", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/ZonaDeContacto"}],
-             [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia"}, {"name" : "Mis cualidades", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades"}, {"name" : "Mis gustos", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos"}, {"name" : "Sueña", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena"}, {"name" : "Punto de encuentro", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"}],
-             [{"name" : "Cabina de soporte", "status" : 0, "link":"/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"}],
-             [{"name" : "Exploración final", "status" : 0, "link":"/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"}]  ];
-             */
+            $scope.theActivities = [    [{"name" : "Exploracion Inicial", "status" : 0, "link" : "/ZonaDeVuelo/ExploracionInicial/zv_exploracionInicial"}],
+                [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/CuartoDeRecursos/FuenteDeEnergia/zv_cuartoderecursos_fuentedeenergia"}],
+                [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/FuenteDeEnergia/zv_conocete_fuentedeenergia"}, {"name" : "Reto múltiple", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/RetoMultiple/zv_conocete_retomultiple"}, {"name" : "Punto de encuentro", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/PuntoDeEncuentro/Topicos/64"}, {"name" : "Zona de contacto", "status" : 0, "link" : "/ZonaDeVuelo/Conocete/ZonaDeContacto"}],
+                [{"name" : "Fuente de energía", "status" : 0, "link" : "/ZonaDeVuelo/MisSuenos/FuenteDeEnergia/zv_missuenos_fuentedeenergia"}, {"name" : "Mis cualidades", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisCualidades/zv_missuenos_miscualidades"}, {"name" : "Mis gustos", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/MisGustos/zv_missuenos_misgustos"}, {"name" : "Sueña", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/Suena/zv_missuenos_suena"}, {"name" : "Punto de encuentro", "status" : 0, "link":"/ZonaDeVuelo/MisSuenos/PuntosDeEncuentro/Topicos/zv_missuenos_puntosdeencuentro"}],
+                [{"name" : "Cabina de soporte", "status" : 0, "link":"/ZonaDeVuelo/CabinaDeSoporte/zv_cabinadesoporte_chat"}],
+                [{"name" : "Exploración final", "status" : 0, "link":"/ZonaDeVuelo/ExploracionFinal/zv_exploracionfinal"}]  ];
 
 
             var activitiesURLs = [
@@ -75,13 +87,21 @@ angular
                 $location.path(activitiesURLs[challenge][activity]);
             };
 
+            $scope.goToUrl2 = function (url) {
+                $location.path(url);
+            };
+
+            //stageId = 1
+
+            $scope.model = JSON.parse(localStorage.getItem("usercourse"));
+
+            $scope.idEtapa = $routeParams['stageId'] - 1; //We are in stage stageId, taken from URL
+            $scope.nombreEtapaActual = $scope.model.stages[$scope.idEtapa].sectionname;
+
             var totalDeEtapas = $scope.model.stages.length; //Total amount of stages
-
-            var totalDeRetos = 0;   //Total number of challenges, along all possible stages
+            var totalDeRetos = 0; //Total number of challenges, along all possible stages
             var totalDeActividades = 0; //Total number of activities, along all challenges in all stages
-
-            //Getting total number of challenges along all stages (1, 2 y 3)
-            var i, j, k;
+            var i, j, k; //Getting total number of challenges along all stages (1, 2 y 3)
 
             for (i = 0; i < totalDeEtapas; i++) {
                 totalDeRetos += $scope.model.stages[i].challenges.length;
@@ -89,19 +109,15 @@ angular
 
             // Count of whole activities along all Stages
             for (i = 0; i < totalDeEtapas; i++) {
-
                 var numOfChallenges = $scope.model.stages[i].challenges.length;
 
                 for (j = 0; j < numOfChallenges; j++) {
                     totalDeActividades += $scope.model.stages[i].challenges[j].activities.length;
                 }
-            } 
-                        
+            }
+
             var avanceEnEtapaActual = 0;
-
-            //Attainment of user in the current Stage
-            var totalActividadesEnEtapaActual = 0;
-
+            var totalActividadesEnEtapaActual = 0; //Attainment of user in the current Stage
             var retosEnEtapaActual = $scope.model.stages[$scope.idEtapa].challenges.length;
 
             for (j = 0; j < retosEnEtapaActual; j++) {
@@ -114,8 +130,6 @@ angular
             }
 
             $scope.avanceEnEtapaActual = Math.ceil(avanceEnEtapaActual * 100 / totalActividadesEnEtapaActual);
-
-
             $scope.retosIconos = {
                 "Exploración Inicial": "assets/images/challenges/stage-1/img-evaluacion inicial.svg",
                 "Cuarto de recursos": "assets/images/challenges/stage-1/img-cuarto-recursos.svg",
@@ -125,28 +139,74 @@ angular
                 "Exploración final": "assets/images/challenges/stage-1/img-evaluacion final.svg"
             };
 
-
             $scope.playVideo = function (videoAddress, videoName) {
                 playVideo(videoAddress, videoName);
             };
 
-            $scope.openClosingStageModal = function (size) {
-                console.log("opening");
-                //setTimeout(function(){ 
-                var modalInstance = $modal.open({
-                    animation: $scope.animationsEnabled,
-                    templateUrl: 'ClosingStage.html',
-                    controller: 'closingStageController',
-                    size: size,
-                    windowClass: 'closing-stage-modal user-help-modal'
-                });
-                console.log("modal open closing");
-                //}, 1000);
-            };
+            $scope.startActivity = function (activity) {
+                var currentUser = JSON.parse(localStorage.getItem("CurrentUser"));
+                // Temporary
+                activity = {
+                    groupid:"10_-1_ActivityManager",
+                    parentsection:1,
+                    section:10,
+                    sectionname:"Cabina de soporte",
+                    activityname:"Cabina de soporte",
+                    activity_type:"ActivityManager",
+                    activityintro:"",
+                    coursemoduleid:68,
+                    points:400,
+                    activity_identifier:"1002",
+                    courseid:4,
+                    status:0,
+                    firsttime:0,
+                    last_status_update:null,
+                    optional:0,
+                    activities:[]
+                };
 
+                var data = {
+                    userId: currentUser.userId,
+                    datestarted: "",
+                    moduleid: activity.coursemoduleid,
+                    updatetype: 1
+                };
+
+                moodleFactory.Services.PutStartActivity(data, activity, currentUser.token function () {
+                    setTimeout(function() { 
+                        var modalInstance = $modal.open({
+                            animation: $scope.animationsEnabled,
+                            templateUrl: 'cabinaSoporteMsj.html',
+                            controller: function ($scope, $modalInstance) {
+                                $scope.cancel = function () {
+                                    $modalInstance.dismiss('cancel');
+                                };
+                            },
+                            size: size,
+                            windowClass: 'user-help-modal'
+                        });
+
+                        console.log("modal open");
+                    }, 500);
+                });
+            };
+        
+            function openStageModal(){
+                    console.log("opening");
+                    //setTimeout(function(){ 
+                    var modalInstance = $modal.open({
+                        animation: $scope.animationsEnabled,
+                        templateUrl: 'ClosingStage.html',
+                        controller: 'closingStageController',
+                        //size: size,
+                        windowClass: 'closing-stage-modal user-help-modal'
+                    });
+                    console.log("modal open closing");
+                    //}, 1000);
+                };
         }])
-    .controller('closingStageController', function ($scope, $modalInstance) {
-        $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
-        };
-    });
+        .controller('closingStageController', function ($scope, $modalInstance) {
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        });
