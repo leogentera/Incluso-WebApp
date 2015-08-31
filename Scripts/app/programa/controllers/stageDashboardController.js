c9467956f5ea00a6ef497a29eddb793b17499380
Freddy Castro <freddy.castro@definityfirst.com>
2015-08-30 20:43:47 -0700

    Added logic to block starting activities if required activities haven't been completed

--- Scripts/app/programa/controllers/stageDashboardController.js
+++ Scripts/app/programa/controllers/stageDashboardController.js
@@ -86,6 +86,7 @@ angular
             var stageProgressBuffer = 0;
             var stageTotalActivities = 0; //Attainment of user in the current Stage
             var stageChallengesCount = $scope.thisStage.challenges.length;
+            $scope.activityStatus = {};
             var i, j,k;
             for (i = 0; i < stageChallengesCount; i++) {
                 var challenge = $scope.thisStage.challenges[i];
@@ -94,6 +95,7 @@ angular
                     var activity = challenge.activities[j];
                     stageProgressBuffer += activity.status;
                     stageTotalActivities++;
+                    $scope.activityStatus[activity.coursemoduleid] = activity.status;
                     if(activity.activities) {
                         var subActivitiesCount = activity.activities.length;
                         for (k = 0; k < subActivitiesCount; k++) {
@@ -120,6 +122,25 @@ angular
 
             $scope.playVideo = function (videoAddress, videoName) {
                 playVideo(videoAddress, videoName);
+            };
+
+            $scope.canStartActivity = function(activity,index,parentIndex){
+                if(!activity.status) {
+                    var activityDependenciesRecord = _.filter(_activityDependencies, function (x) {
+                        return x.id == activity.coursemoduleid
+                    });
+                    if (activityDependenciesRecord[0]) {
+                        var activityDependencies = activityDependenciesRecord[0].dependsOn;
+                        var dependenciesCount = activityDependencies.length;
+                        for (var i = 0; i < dependenciesCount; i++) {
+                            if (!$scope.activityStatus[activityDependencies[i]]) {
+                                console.log("Cannot start activity yet");
+                                return false;
+                            }
+                        }
+                    }
+                }
+                $scope.startActivity(activity,index,parentIndex);
             };
 
             $scope.startActivity = function (activity, index, parentIndex) {
