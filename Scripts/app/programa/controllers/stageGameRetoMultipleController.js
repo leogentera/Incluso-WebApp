ec69c8735c2f17afd896f5bb31f296bcb6db1d17
gutierfe <fernando@sieena.com>
2015-09-06 23:19:32 -0500

    rolledback

--- Scripts/app/programa/controllers/stageGameRetoMultipleController.js
+++ Scripts/app/programa/controllers/stageGameRetoMultipleController.js
@@ -180,14 +180,8 @@ angular
                 if (parentActivity.status == 0) {
                   if (shield != "" && $scope.profile) {
                     //update profile
-                    var currentUser = JSON.parse(moodleFactory.Services.GetCacheObject("CurrentUser"));
-
                     $scope.profile["shield"] = shield;
-                    currentUser.shield = shield;
-
                     _setLocalStorageJsonItem("profile", $scope.profile);
-                    _setLocalStorageJsonItem("CurrentUser", currentUser)
-
                     $scope.saveUser();
                   }
 
