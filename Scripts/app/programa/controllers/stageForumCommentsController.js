eed28be544e0779dc325e22c8b1a4bfc602fd70d
Bernardo Garza Garc�a <bernardo.garza@sieena.com>
2015-09-08 12:17:47 -0500

    se mejora implementación en mostrar limite de comentarios

--- Scripts/app/programa/controllers/stageForumCommentsController.js
+++ Scripts/app/programa/controllers/stageForumCommentsController.js
@@ -335,7 +335,8 @@ angular
             var initializeCommentsData = function(element, index, array){
                 $scope.isCommentModalCollapsed[index] = false;
                 console.log(element.post_id);
-                $scope.showAllCommentsByPost[element.post_id] = false;
+                
+                $scope.showAllCommentsByPost[element.post_id] = 3;
             };
 
             var refreshTopicData = function(){   
@@ -356,7 +357,7 @@ angular
             
             $scope.showPreviousComments = function(postId) {
             
-                $scope.showAllCommentsByPost[postId] = true;
+                $scope.showAllCommentsByPost[postId] = 1000000;
             };
             
             $scope.showMore = function() {
