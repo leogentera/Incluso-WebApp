c1452ac580727090f96002700803ded2d9aa8bdc
Guillermo <guillermo.ulloa@definityfirst.com>
2015-09-08 13:26:18 -0500

    Fixed issue related to carousel not working at "Zona de Vuelo" stage.

--- Scripts/app/programa/controllers/stageDashboardController.js
+++ Scripts/app/programa/controllers/stageDashboardController.js
@@ -34,80 +34,140 @@ angular
             var hits = 1;
 
                 //Carrusel de retos
-                var owl2 = $("#owl-demo2");
+               //  var owl2 = $("#challengesCarousel");
 
-                owl2.owlCarousel({
-                    navigation: false,
-                    pagination: false,
-                    //paginationSpeed: 1000,
-                    goToFirstSpeed: 2000,
-                    singleItem: true,
-                    autoHeight: true,
-                    touchDrag:false,
-                    mouseDrag:false,
-                    transitionStyle:"fade",
-                    afterMove: callback1
-                });
+               //  owl2.owlCarousel({
+               //      navigation: false,
+               //      pagination: false,
+               //      //paginationSpeed: 1000,
+               //      goToFirstSpeed: 2000,
+               //      singleItem: true,
+               //      autoHeight: true,
+               //      touchDrag:false,
+               //      mouseDrag:false,
+               //      transitionStyle:"fade",
+               //      afterMove: callback1
+               //  });
 
-                //Carrusel de Actividades
-                var owl = $("#owl-demo");
+               //  //Carrusel de Actividades
+               //  var owl = $("#activitiesCarousel");
 
-                owl.owlCarousel({
-                    navigation: false,
-                    pagination: false,
-                    //paginationSpeed: 1000,
-                    goToFirstSpeed: 2000,
-                    singleItem: true,
-                    autoHeight: true,
-                    touchDrag:false,
-                    mouseDrag:false,
-                    transitionStyle:"fade",
-                    afterMove: callback2
-                });    
+               //  owl.owlCarousel({
+               //      navigation: false,
+               //      pagination: false,
+               //      //paginationSpeed: 1000,
+               //      goToFirstSpeed: 2000,
+               //      singleItem: true,
+               //      autoHeight: true,
+               //      touchDrag:false,
+               //      mouseDrag:false,
+               //      transitionStyle:"fade",
+               //      afterMove: callback2
+               //  });    
 
+               //  this.currentItem = $scope.idReto;
+               //  var currentItem;
+               //  owl.trigger("owl.goTo", $scope.idReto);
+               //      $("span#index").text(($scope.idReto+1));  
+
+               //  owl2.trigger("owl.goTo", $scope.idReto);
+               //      $("span#index").text(($scope.idReto+1));            
+
+               //  function callback1(event) {
+               //      var item = this.currentItem;                    
+               //      currentItem = parseInt(this.owl.currentItem);
+               //      owl2.trigger("owl.goTo", item);
+               //      $("span#index").text((item+1));
+               //  }
+
+               //  function callback2(event) {
+               //      item = this.currentItem;                    
+               //      owl.trigger("owl.goTo", item);
+               //      $("span#index").text((item+1));
+               //  }
+
+               // $("#prev").click(function (ev) {                                                            
+               //      if(currentItem){
+               //          owl.trigger('owl.goTo', currentItem - 1);
+               //          owl2.trigger('owl.goTo', currentItem - 1);
+               //      }
+               //      else{
+               //          owl.trigger('owl.prev');
+               //          owl2.trigger('owl.prev');
+               //      }
+               //      ev.preventDefault();
+               //  });
+               //   $("#next").click(function (ev) {                                        
+               //      if(currentItem){
+               //          owl.trigger('owl.goTo', currentItem + 1);
+               //          owl2.trigger('owl.goTo', currentItem + 1);
+               //      }
+               //      else{
+               //          owl.trigger('owl.next');
+               //          owl2.trigger('owl.next');
+               //      }
+               //      ev.preventDefault();
+               //  });
                 this.currentItem = $scope.idReto;
-                var currentItem;
-                owl.trigger("owl.goTo", $scope.idReto);
-                    $("span#index").text(($scope.idReto+1));  
+                var $challengesCarousel = $("#challengesCarousel");
+                var $activitiesCarousel = $("#activitiesCarousel");
+                var $prev = $("#prev");
+                var $next = $("#next");
 
-                owl2.trigger("owl.goTo", $scope.idReto);
-                    $("span#index").text(($scope.idReto+1));            
-
-                function callback1(event) {
-                    var item = this.currentItem;                    
-                    currentItem = parseInt(this.owl.currentItem);
-                    owl2.trigger("owl.goTo", item);
-                    $("span#index").text((item+1));
+                var prevItem = function(e) {
+                    $challengesCarousel.trigger('owl.prev');
+                    $activitiesCarousel.trigger('owl.prev');
+                }
+                var nextItem = function(e) {
+                    $challengesCarousel.trigger('owl.next');
+                    $activitiesCarousel.trigger('owl.next');                    
                 }
 
-                function callback2(event) {
-                    item = this.currentItem;                    
-                    owl.trigger("owl.goTo", item);
-                    $("span#index").text((item+1));
+                var callback1 = function(e) {
+                    var item = this.currentItem;
+                    $challengesCarousel.trigger("owl.goTo", item);
+                }
+                var callback2 = function(e) {
+                    var item = this.currentItem;
+                    $$activitiesCarousel.trigger("owl.goTo", item);
                 }
 
-               $("#prev").click(function (ev) {                                                            
-                    if(currentItem){
-                        owl.trigger('owl.goTo', currentItem - 1);
-                        owl2.trigger('owl.goTo', currentItem - 1);
-                    }
-                    else{
-                        owl.trigger('owl.prev');
-                        owl2.trigger('owl.prev');
-                    }
-                    ev.preventDefault();
-                });
-                 $("#next").click(function (ev) {                                        
-                    if(currentItem){
-                        owl.trigger('owl.goTo', currentItem + 1);
-                        owl2.trigger('owl.goTo', currentItem + 1);
-                    }
-                    else{
-                        owl.trigger('owl.next');
-                        owl2.trigger('owl.next');
-                    }
-                    ev.preventDefault();
-                });
+                var initChallengesCarousel = function() {
+                    $challengesCarousel.owlCarousel({
+                        navigation: false,
+                        pagination: false,
+                        goToFirstSpeed: 2000,
+                        singleItem: true,
+                        autoHeight: true,
+                        touchDrag:false,
+                        mouseDrag:false,
+                        transitionStyle:"fade",
+                        afterMove: null //callback1
+                    });
+                    //- $challengesCarousel.owlCarousel();
+                }
+                var initActivitiesCarousel = function() {
+                    $challengesCarousel.owlCarousel({
+                        navigation: false,
+                        pagination: false,
+                        goToFirstSpeed: 2000,
+                        singleItem: true,
+                        autoHeight: true,
+                        touchDrag:false,
+                        mouseDrag:false,
+                        transitionStyle:"fade",
+                        afterMove: null //callback2
+                    });
+                    //- $challengesCarousel.owlCarousel();
+                }
+                var initControlsBinding = function() {
+                    $prev.click(nextItem);
+                    $next.click(nextItem);
+                }
+
+                initChallengesCarousel();
+                initActivitiesCarousel();
+                initControlsBinding();
 
             }, 1000);         
 
