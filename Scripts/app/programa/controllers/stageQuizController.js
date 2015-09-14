7cf6a248a29cea18c85d18fff1c9a93bf78456d4
Carlos Wilson <carlos.wilson@definityfirst.com>
2015-09-14 14:29:45 -0500

    INC-2277

--- Scripts/app/programa/controllers/stageQuizController.js
+++ Scripts/app/programa/controllers/stageQuizController.js
@@ -1205,6 +1205,48 @@ angular
             };
 
 
+            $scope.validateZNExploracionFinal = function() {
+
+                var cont = $scope.exploracionFinalStage2.length;
+                var quizIsValid = true;
+
+                //Check if first (yes/no) question was answered
+                if ($scope.exploracionFinalStage2[0]) {
+
+                    //Check if second (multiple choice) question was answered
+                    if ($scope.exploracionFinalStage2[1].indexOf(true) > -1) {
+
+                        //Check if third (multiple choice) question was answered
+                        if ($scope.exploracionFinalStage2[2].indexOf(true) > -1) {
+
+                            //Check if fourth (multiple choice) question was answered
+                            if (!($scope.exploracionFinalStage2[3].indexOf(true) > -1)) {
+                                quizIsValid = false;
+                            }
+
+                        } else {
+                            quizIsValid = false;
+                        }
+
+                    } else {
+                        quizIsValid = false;
+                    }
+
+                } else {
+                    quizIsValid = false;
+                }
+                console.log(quizIsValid);
+
+                if (quizIsValid) {
+                    $scope.showWarning = false;
+                    $scope.navigateToPage(2);
+                    $scope.scrollToTop();
+                } else {
+                    showWarningAndGoToTop();
+                }
+            };
+
+
             function partialSuccessfullCallBack(partialActivityAnswers) {
                 if (partialActivityAnswers != null) {
 
