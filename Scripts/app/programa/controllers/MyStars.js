82277886ab9ff6250c71b34cf0bde165fe02a0d1
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-09-15 20:51:43 -0500

    Mostrar total de estrellas del usuario en Mis Estrellas

--- Scripts/app/programa/controllers/MyStars.js
+++ Scripts/app/programa/controllers/MyStars.js
@@ -12,6 +12,16 @@ angular
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $modal) {
             $scope.$emit('ShowPreloader'); //show preloader
             $scope.setToolbar($location.$$path,"Mis estrellas");
+            
+            
+            var profile = JSON.parse(localStorage.getItem("profile"));
+            
+            if (profile) {
+                $scope.profileStars = profile.stars;
+            }else{
+                $scope.profileStars = 0;
+            }
+            
             $rootScope.showFooter = true; 
             $rootScope.showFooterRocks = false; 
             $scope.back = function () {
