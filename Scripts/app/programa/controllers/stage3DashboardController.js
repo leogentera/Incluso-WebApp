1294e9f16612e86e6a4d82df93e5be38a264bbff
Freddy Castro <freddy.castro@definityfirst.com>
2015-09-27 21:54:34 -0500

    Removed override on blocking activities

--- Scripts/app/programa/controllers/stage3DashboardController.js
+++ Scripts/app/programa/controllers/stage3DashboardController.js
@@ -223,8 +223,7 @@ angular
             };
 
             $scope.startActivity = function (activity, index, parentIndex) {
-                //TODO: Remove false from condition, only there to jump freely into activities in DEV
-                if(false && _activityBlocked[activity_identifier].disabled) return false;
+                if(_activityBlocked[activity.activity_identifier].disabled) return false;
                 var url = _.filter(_activityRoutes, function(x) { return x.id == activity.activity_identifier })[0].url;
 
                 if (url) {
