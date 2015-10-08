52dd018cdba699c73868185ab14a8af9ac34de97
Bernardo Garza Garc�a <bernardo.garza@sieena.com>
2015-10-08 13:50:40 -0500

    se añaden funciones para realizar solicitudes a servicio de drupal

---
+++ Scripts/app/shared/services/drupalServices.js
@@ -0,0 +1,48 @@
+(function () {
+    namespace('drupalFactory');
+
+    drupalFactory.Services = (function () {
+        
+        var API_RESOURCE = "http://app.incluso.com.mx/drupal/rest/node/{0}"; /* Nora */
+        
+        var _getContent = function (activityIdentifierId, sucessCallback, errorCallback, forceRefresh) {
+            
+            _getAsyncData("drupal_content_" + activityIdentifierId, API_RESOURCE.format(activityIdentifierId), successCallback, errorCallback, forceRefresh);
+        };
+        
+        var _getAsyncData = function (key, url, activityIdentifierId, successCallback, errorCallback, forceRefresh) {
+            
+            var returnValue = (forceRefresh) ? null : _getCacheJson(key);
+
+            if (returnValue) {
+                _timeout(function () { successCallback(returnValue, key) }, 1000);
+                return returnValue;
+            }
+
+            _httpFactory({
+                method: 'GET',
+                url: url,
+                headers: { 'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                _setLocalStorageJsonItem(key, data);
+                successCallback(data, key);
+            }).error(function (data, status, headers, config) {
+                errorCallback(data);
+            });
+        };
+        
+        var _getCacheJson = function (key) {
+            var str = localStorage.getItem(key);
+            if (str == null) {
+                return null;
+            } else {
+                return JSON.parse(str);
+            }
+        };
+
+        return {
+            GetContent: _getContent,
+        };
+    })();
+    
+}).call(this);
