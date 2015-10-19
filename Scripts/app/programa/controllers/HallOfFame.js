d51b0e5ffe37324c71539fc760ddb4ef519998ab
eberflores <eber.flores@definityfirst.com>
2015-10-19 18:33:12 -0500

    Salón de la fama funcional

--- Scripts/app/programa/controllers/HallOfFame.js
+++ Scripts/app/programa/controllers/HallOfFame.js
@@ -12,9 +12,9 @@ hallOfFameModule
         '$modal',
         function ($q, $scope, $location, $routeParams, $timeout, $rootScope, $http, $modal) {
 
+            $scope.$emit('ShowPreloader');
             _httpFactory = $http;
             _timeout = $timeout;
-
             $scope.setToolbar($location.$$path,"Incluso");
             $rootScope.showFooter = true;
             $rootScope.showFooterRocks = false;
@@ -24,56 +24,17 @@ hallOfFameModule
             $scope.back = function () {
                 $location.path('/ProgramaDashboard');
             };
-            $scope.$emit('ShowPreloader');
-
-
-
-            $scope.cities =
-            [
-                "Ver Todo",
-                "Aguascalientes",
-                "Baja California",
-                "Baja California Sur",
-                "Campeche",
-                "Chiapas",
-                "Chihuahua",
-                "Coahuila",
-                "Colima",
-                "DF",
-                "Durango",
-                "Estado de México",
-                "Guanajuato",
-                "Guerrero",
-                "Hidalgo",
-                "Jalisco",
-                "Michoacán",
-                "Morelos",
-                "Nayarit",
-                "Nuevo León",
-                "Oaxaca",
-                "Puebla",
-                "Querétaro",
-                "Quintana Roo",
-                "San Luis Potosí",
-                "Sinaloa",
-                "Sonora",
-                "Tabasco",
-                "Tamaulipas",
-                "Tlaxcala",
-                "Veracruz",
-                "Yucatán",
-                "Zacatecas"
-            ];
+            var citiesCatalogKey = "citiescatalog";
             $scope.user = moodleFactory.Services.GetCacheJson("CurrentUser");
-
-            moodleFactory.Services.GetAsyncCatalog("citiescatalog",$scope.user.token,function(){},function(data){console.log(data)},true);
+            $scope.usercourse = JSON.parse(localStorage.getItem("usercourse"));
+            moodleFactory.Services.GetAsyncCatalog(citiesCatalogKey,$scope.user.token,getCitiesCatalogCallback,function(data){console.log(data)},true);
+            $scope.default = true;
             getTop5("Ver Todo");
             var userId = localStorage.getItem("userId");
             var profileKey = "profile/" + userId;
             var jsonProfile = localStorage.getItem(profileKey);
             var profile = JSON.parse(jsonProfile);
             var imageUrl = $scope.user.profileimageurl;
-
             var userStats =
             {
                 profileImageUrl: imageUrl,
@@ -85,60 +46,20 @@ hallOfFameModule
             };
             $scope.userStats = userStats;
 
-            //var topProgress = [];
-            //var topStars = [];
-            //var topBadges = [];
-            //var topContribution = [];
-            //for(var i=0;i<5;i++)   //Dummies under construction
-            //{
-            //    var newUserProgress =
-            //    {
-            //        profileimageurl : imageUrl,
-            //        alias: "UserByProgress" + (i+1),
-            //        rank: i+1,
-            //        progress: parseInt(Math.round(Math.random() * 10) + (90 - i * 10))
-            //    }
-            //    topProgress.push(newUserProgress);
-            //
-            //    var newUserStars =
-            //    {
-            //        profileimageurl : imageUrl,
-            //        alias: "UserByStars" + (i+1),
-            //        rank: i+1,
-            //        stars: parseInt(parseInt(Math.round(Math.random() * 100))  + (1000 - i*100) )
-            //    }
-            //    topStars.push(newUserStars);
-            //
-            //    var newUserBadges =
-            //    {
-            //        profileimageurl : imageUrl,
-            //        alias: "UserByBadges" + (i+1),
-            //        rank: i+1,
-            //        amount:parseInt(Math.random()*5 + 50 - (i * 5))
-            //    }
-            //    topBadges.push(newUserBadges);
-            //
-            //    var newUserContribution =
-            //    {
-            //        profileimageurl : imageUrl,
-            //        alias: "UserByCotribution" + (i+1),
-            //        rank: i+1,
-            //        forum:parseInt(50 - 10 * i + Math.random() * 10),
-            //        messages:parseInt(50 - 10 * i + Math.random()*10),
-            //        community:parseInt(50 - 10 * i + Math.random()*10)
-            //    }
-            //    topContribution.push(newUserContribution);
-            //}
-            //
-            //$scope.topProgress = topProgress;
-            //$scope.topStars = topStars;
-            //$scope.topBadges = topBadges;
-            //$scope.topContribution = topContribution;
 
             function getTop5(city)
             {
-                $scope.usercourse = JSON.parse(localStorage.getItem("usercourse"));
-                moodleFactory.Services.GetAsyncHallOfFame($scope.usercourse.courseid,city,$scope.user.token,getTop5Callback, function(data){console.log(data)})
+                moodleFactory.Services.GetAsyncHallOfFame($scope.usercourse.courseid,city,$scope.user.token,getTop5Callback, function(data){console.log(data)},true)
+            }
+
+            function getCitiesCatalogCallback()
+            {
+                $scope.cities = moodleFactory.Services.GetCacheJson(citiesCatalogKey);
+                //FIND ME Temporary
+                $scope.cities = ["M\u00e9xico D.F","Estado de M\u00e9xico","OTRO"];
+                $scope.cities.unshift("Ver Todo");
+
+
             }
 
             function getTop5Callback()
@@ -149,14 +70,23 @@ hallOfFameModule
                 $scope.topBadges = allTop5.badges;
                 $scope.topContribution = allTop5.activity;
 
+
                 $scope.userStats.forum = allTop5.current_user_activity.forum;
                 $scope.userStats.messages = allTop5.current_user_activity.messages;
                 $scope.userStats.comunity = allTop5.current_user_activity.comunity;
-
-
                 $scope.$emit('HidePreloader');
             }
 
+            $scope.updateByCity = function()
+            {
+                if($scope.selectedCity != "Ver Todo" || !$scope.default)
+                {
+                    $scope.$emit('ShowPreloader');
+                    getTop5($scope.selectedCity);
+                    $scope.default = false;
+                }
+
+            }
 }]);
 
 hallOfFameModule.directive('progressBar', function($compile)
