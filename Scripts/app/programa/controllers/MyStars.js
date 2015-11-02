6f0286424bc2c933ee6c27e5758bf52ae86fd02c
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-11-01 18:28:45 -0600

    Refactor en mis estrellas

--- Scripts/app/programa/controllers/MyStars.js
+++ Scripts/app/programa/controllers/MyStars.js
@@ -42,20 +42,58 @@ angular
             function addStarsByActivity(data){
                 
                 var starsByActivity = [];                
-                for(var i=0; i < data.length; i++){
-                    console.log(data[i]);
+                //for(var i=0; i < data.length; i++){
+                //    console.log(data[i]);
+                //    if (data[i].points != 0) {
+                //        var courseModuleId = data[i].instance;                        
+                //        var activityManagers = JSON.parse(localStorage.getItem("activityManagers"));                       
+                //        for(j = 0; j < activityManagers.length; j++){
+                //            
+                //            for(k=0; k < activityManagers[j].activities.length; k++){
+                //                if (activityManagers[j].activities[k].coursemoduleid == courseModuleId) {                                    
+                //                    starsByActivity.push(activityManagers[j].activities[k]);
+                //                }
+                //            }                            
+                //        }
+                //    }       
+                //}
+                var points=0;
+                for(var i=0; i< data.length; i++){
                     if (data[i].points != 0) {
-                        var courseModuleId = data[i].instance;                        
-                        var activityManagers = JSON.parse(localStorage.getItem("activityManagers"));                       
-                        for(j = 0; j < activityManagers.length; j++){                        
-                            for(k=0; k < activityManagers[j].activities.length; k++){
-                                if (activityManagers[j].activities[k].coursemoduleid == courseModuleId) {                                    
-                                    starsByActivity.push(activityManagers[j].activities[k]);
+                        var courseModuleId = data[i].instance;
+                        var userCourse = JSON.parse(localStorage.getItem("usercourse"));
+                        var stages = userCourse.stages;
+                        for(var j=0; j < stages.length; j++){
+                            var challenges = stages[j].challenges;
+                            for(var k = 0; k < challenges.length ; k++){
+                                var challengeName = challenges[k].activityname;
+                                var activities = challenges[k].activities;
+                                for(var l=0; l < activities.length; l++){
+                                    var activity = activities[l];
+                                    if (activity.coursemoduleid == courseModuleId) {
+                                        activity.sectionname = challengeName;
+                                        starsByActivity.push(activity);
+                                        points = points + activity.points;
+                                    }
+                                    else{
+                                        if (activity.activities) {                                        
+                                            for(var m=0; m< activity.activities.length; m++){
+                                                var subactivity = activity.activities[m];
+                                                if (subactivity.coursemoduleid == courseModuleId){
+                                                    subactivity.sectionname = challengeName;
+                                                    subactivity.activityname = activity.activityname;
+                                                    starsByActivity.push(subactivity)
+                                                    points = points + subactivity.points;
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
-                            }                            
+                            }
                         }
-                    }       
+                    }
                 }
+                console.log(points);
                                 
                 $scope.activitiesCompleted = _.sortBy(starsByActivity, function(act){
                     return act.last_status_update;
