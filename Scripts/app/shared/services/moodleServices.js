8797a2f7362bbdcc2895124231994b61cb131fda
met1887 <met_1887@hotmail.com>
2015-11-03 16:26:38 -0600

    Fuente de Energia - Fix merge

--- Scripts/app/shared/services/moodleServices.js
+++ Scripts/app/shared/services/moodleServices.js
@@ -31,10 +31,6 @@
             _getAsyncData("activity/" + activityArray, API_RESOURCE.format('activity?' + activityArray), null,  successCallback, errorCallback, forceRefresh);
         };
 
-        var _getAsyncActivitiesEnergy = function (activityArray, successCallback, errorCallback, forceRefresh) {
-            _getAsyncData("activity/" + activityArray, API_RESOURCE.format('activity?' + activityArray), successCallback, errorCallback, forceRefresh);
-        };
-
         var _getAsyncForumInfo = function (activityId, token, successCallback, errorCallback, forceRefresh) {
             _getForumAsyncData("activity/" + activityId, API_RESOURCE.format('activity/' + activityId), token, successCallback, errorCallback, forceRefresh);
         };
@@ -906,7 +902,6 @@
             GetAsyncActivity: _getAsyncActivityInfo,
             GetAsyncActivitiesEnergy: _getAsyncActivitiesEnergy,
             GetAsyncActivities: _getAsyncActivitiesInfo,
-            GetAsyncActivitiesEnergy: _getAsyncActivitiesEnergy,
             GetAsyncActivityQuizInfo: _getAsyncActivityQuizInfo,
             PutAsyncQuiz: _putAsyncQuiz,
             GetAsyncForumInfo: _getAsyncForumInfo,
