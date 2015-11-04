9b6a7e324137ee2f3ef3a7f5fdef59757621a46a
eberflores <eber.flores@definityfirst.com>
2015-11-03 18:31:44 -0600

    Closing message content from drupal

--- Scripts/app/programa/controllers/stageContentsController.js
+++ Scripts/app/programa/controllers/stageContentsController.js
@@ -409,6 +409,20 @@ angular
                     $rootScope.pageName = $scope.contentResources.title_toolbar;
                     
                     }, function () {}, true);
+
+                /*Closing message content*/
+                var stageClosingContent = "";
+                if(activityIdentifierId > 999 && activityIdentifierId < 2000)
+                    stageClosingContent = "ZonaDeVueloClosing";
+                else if(activityIdentifierId > 1999 && activityIdentifierId < 3000)
+                    stageClosingContent = "ZonaDeNavegacionClosing";
+                else
+                    stageClosingContent = "ZonaDeAterrizajeClosing";
+
+                drupalFactory.Services.GetContent(stageClosingContent, function (data, key)
+                {
+                    $scope.closingContent = data.node;
+                }, function () { }, true);
             }
             console.log(moduleid);
             getContentResources(moduleid);
