989f89941ea8ac42c465a2277a7d3b3e5f9e24a3
Luis Forero <foluis@hotmail.com>
2015-11-05 11:34:06 -0600

    Fix when saving profile gender error

--- Scripts/app/programa/controllers/profile.js
+++ Scripts/app/programa/controllers/profile.js
@@ -523,8 +523,8 @@ function formatDate(date) {
                 if (age < 13) { errors.push("Debes ser mayor de 13 años para poder registrarte."); }
                 if (!$scope.editForm.firstname.$valid) { errors.push("Formato de nombre incorrecto."); }
                 if (!$scope.editForm.lastname.$valid) { errors.push("Formato de apellido paterno incorrecto."); }
-                if (!$scope.editForm.mothername.$valid) { errors.push("Formato de apellido materno incorrecto."); }
-                if (!$scope.editForm.gender.$valid) { errors.push("Debe indicar su género."); }
+                if (!$scope.editForm.mothername.$valid) { errors.push("Formato de apellido materno incorrecto."); }                
+                if (!$scope.model.gender) { errors.push("Debe indicar su género."); }
                 if (!isValidDate($scope.model.birthday)) { errors.push("Ingrese la fecha de nacimiento."); }
 
                 // ************************ The following are not required fields. ****************************
