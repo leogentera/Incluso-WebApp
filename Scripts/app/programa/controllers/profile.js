2550028a053f00002266b8d88c0ea6f321dcdc16
Carlos Wilson <carlos.wilson@definityfirst.com>
2015-12-01 17:05:23 -0600

    Updated date validation in profile.js

--- Scripts/app/programa/controllers/profile.js
+++ Scripts/app/programa/controllers/profile.js
@@ -531,8 +531,8 @@ angular
                 function isValidDate(date) {
                     var matches = /^(\d{2})[-\/](\d{2})[-\/](\d{4})$/.exec(date);
                     if (matches == null) return false;
-                    var m = matches[1] - 1;
-                    var d = matches[2];
+                    var m = matches[2] - 1;
+                    var d = matches[1];
                     var y = matches[3];
                     var composedDate = new Date(y, m, d);
                     return composedDate.getDate() == d &&
