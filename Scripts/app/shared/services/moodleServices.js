4c946282f90b2b93050f2420caa16bd883695bdd
UlisesBecerra <ulises.becerra@definityfirst.com>
2015-12-08 12:43:41 -0600

    Agregar token de usuario en putAsyncData

--- Scripts/app/shared/services/moodleServices.js
+++ Scripts/app/shared/services/moodleServices.js
@@ -449,7 +449,8 @@
                 method: 'PUT',
                 url: url,
                 data: dataModel,
-                headers: { 'Content-Type': 'application/json' }
+                headers: { 'Content-Type': 'application/json' ,
+                           'Authorization': currentUser.token }
             });
             _setLocalStorageJsonItem(key,dataModel);
 
