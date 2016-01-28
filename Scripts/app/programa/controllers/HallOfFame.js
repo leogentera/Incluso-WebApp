4f00017973846989a7cbb454f4f4c67b3172d817
Carlos Wilson <carlos.wilson@definityfirst.com>
2016-01-27 18:26:26 -0600

    Removd unused directive from Hall of Fame controller

--- Scripts/app/programa/controllers/HallOfFame.js
+++ Scripts/app/programa/controllers/HallOfFame.js
@@ -186,24 +186,5 @@ hallOfFameModule.filter('numberToCardinal', function () {
         };
     });
 
-hallOfFameModule.directive('errSrc', function() {
-    return {
-        link: function(scope, element, attrs) {
-
-            scope.$watch(function() {
-                return attrs['ngSrc'];
-            }, function (value) {
-                if (!value) {
-                    element.attr('src', attrs.errSrc);
-                }
-            });
-
-            element.bind('error', function() {
-                element.attr('src', attrs.errSrc);
-            });
-        }
-    }
-});
-
 
 
