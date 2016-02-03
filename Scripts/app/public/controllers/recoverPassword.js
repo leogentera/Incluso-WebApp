6cbef64d444f2ed8158d7d3edf43d9f9872d8afa
Carlos Wilson <carlos.wilson@definityfirst.com>
2016-02-03 14:49:05 -0600

    Added Improvements 1.21, 1.22 and 1.24

--- Scripts/app/public/controllers/recoverPassword.js
+++ Scripts/app/public/controllers/recoverPassword.js
@@ -24,10 +24,11 @@ angular
             /* ViewModel */
             $scope.recoverPasswordModel = {
                 email: "",
+                userOrEmail: undefined,
                 secretQuestion: "",
                 secretAnswer: "",
-                password: "",
-                confirmPassword: "",
+                password: undefined,
+                confirmPassword: undefined,
                 code: "",
                 modelState: {
                     isValid: null,
@@ -57,53 +58,104 @@ angular
                 $scope.recoverPasswordModel.modelState.isValid = (newValue.length === 0);
             });
 
-            function offlineCallback() {
-                $timeout(function () {
-                    $scope.recoverPasswordModel.modelState.errorMessages = ["Se necesita estar conectado a Internet para continuar."];
-                    $scope.$emit('scrollTop');
-                }, 1000);
-            }
+            $scope.$watch("recoverPasswordModel.password", function (newValue, oldValue) {
+                var passWordPattern = /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)(?=.*[_\W])[\S]{8,}$/;
+                $scope.passwordSignal = passWordPattern.test(newValue) && $scope.recoverPasswordModel.password;
+                $scope.confirmPasswordSignal = $scope.recoverPasswordModel.confirmPassword == $scope.recoverPasswordModel.password && $scope.recoverPasswordModel.confirmPassword && $scope.passwordSignal;
+            });
 
-            function checkEqualityOfPasswords(password, confirmPassword) {
-                if (password === confirmPassword) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
+            $scope.$watch("recoverPasswordModel.confirmPassword", function (newValue, oldValue) {
+                var passWordPattern = /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)(?=.*[_\W])[\S]{8,}$/;
+                $scope.passwordSignal = passWordPattern.test($scope.recoverPasswordModel.password) && $scope.recoverPasswordModel.password;
+                $scope.confirmPasswordSignal = $scope.recoverPasswordModel.confirmPassword == $scope.recoverPasswordModel.password && $scope.recoverPasswordModel.confirmPassword && $scope.passwordSignal;
+            });
 
-            $scope.login = function () {
+            $scope.backToLogin = function () {
+                $scope.$emit('ShowPreloader');
                 $location.path('/');
+            };
+
+            $scope.openModalPassword = function (size) {
+                var modalInstance = $modal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: 'passwordInfoModal.html',
+                    controller: 'termsAndConditionsController',
+                    size: size,
+                    windowClass: 'modal-theme-default terms-and-conditions',
+                    backdrop: 'static'
+                });
+            };
+
+            $scope.togglePassword = function () {
+                var inputPassword = $("#password");
+                var inputConfirmPassword = $("#confirmPassword");
+
+                if (inputPassword.attr("type") == "text") {
+                    inputPassword.attr("type", "password");
+                    inputConfirmPassword.attr("type", "password");
+                } else {
+                    inputPassword.attr("type", "text");
+                    inputConfirmPassword.attr("type", "text");
+                }
+            };
+
+            $scope.backToProfile = function () {
+                $location.path('/Perfil/' + moodleFactory.Services.GetCacheObject("userId"));
             };
 
             $scope.navigateToPage = function (pageNumber) {
                 $scope.currentPage = pageNumber;
-                $scope.$emit('scrollTop');
+                $scope.$emit('scrollTop'); //- scroll
             };
 
-            $scope.getPasswordRecoveryCode = function () {
-                $scope.validateConnection(getPasswordRecoveryCodeConnectedCallback, offlineCallback);
+            // For page 1 / 3.
+            $scope.validateUserOrEmail = function () {
+                $scope.validateConnection(recoverSecretQuestion, offlineCallback);
             };
 
-            function getPasswordRecoveryCodeConnectedCallback() {
-                //Start Code Recovery
-                //fetching errors list
+            function recoverSecretQuestion() {
+
+                $scope.$emit('ShowPreloader');
+
+                $http({
+                    method: 'POST',
+                    url: API_RESOURCE.format("authentication"),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: $.param({
+                        email: $scope.recoverPasswordModel.userOrEmail,
+                        action: "validate"
+                    })
+                }).success(function (data, status, headers, config) {
+                    $scope.$emit('HidePreloader'); //hide preloader
+                    $scope.currentPage = 2;
+                    $scope.email = data.email;
+                    $scope.secretQuestion = data.secretquestion;
+                    $scope.recoverPasswordModel.secretQuestion = data.secretquestion;
+                    $scope.$emit('scrollTop');
+
+                }).error(function (data, status, headers, config) {
+                    $scope.$emit('HidePreloader');
+                    var errorMessage;
+
+                    if ((data != null && data.messageerror != null)) {
+                        errorMessage = window.atob(data.messageerror);
+                    } else {
+                        errorMessage = "Problema con la red, asegúrate de tener Internet e intenta de nuevo.";
+                    }
+
+                    $scope.recoverPasswordModel.modelState.errorMessages = [errorMessage];
+                    $scope.$emit('scrollTop');
+                });
+            }
+
+            // For page 2 / 3.
+            $scope.validateSecretAnswer = function () {
+                $scope.validateConnection(validateSecretAnswerCallback, offlineCallback);
+            };
+
+            function validateSecretAnswerCallback() {
                 var errors = [];
-                if (!$scope.recoverPasswordForm.email.$valid) {//$error.pattern || $scope.recoverPasswordForm.email.$error.required) {
-                    errors.push("Formato de correo incorrecto.");
-                }
-                if (!$scope.recoverPasswordModel.secretQuestion) {
-                    errors.push("Pregunta secreta inválida.");
-                }
-                if (!$scope.recoverPasswordForm.secretAnswer.$valid) {
-                    errors.push("Respuesta secreta inválida.");
-                }
-                
-                $timeout(function() {
-                    $scope.recoverPasswordModel.modelState.errorMessages = errors;
-                }, 1);
 
-                //validating
                 if (errors.length === 0) {
                     $scope.$emit('ShowPreloader');
 
@@ -112,15 +164,15 @@ angular
                         url: API_RESOURCE.format("authentication"),
                         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                         data: $.param({
-                            email: $scope.recoverPasswordModel.email,
+                            email: $scope.email,
                             secretanswer: $scope.recoverPasswordModel.secretAnswer.toString().toLowerCase(),
                             secretquestion: $scope.recoverPasswordModel.secretQuestion,
                             action: "forgot"
                         })
                     }).success(function (data, status, headers, config) {
-
                         $scope.$emit('HidePreloader');
-                        $scope.currentPage = 2;
+                        $scope.currentPage = 3;
+                        $scope.recoverPasswordModel.modelState.isValid = true; //For activating green success message to user.
                         $scope.successMessage = "Te hemos enviado un correo con un código para recuperar tu contraseña.";
                         $scope.$emit('scrollTop');
 
@@ -141,74 +193,71 @@ angular
                 }
             }
 
-            // For page 2/2
-            $scope.recover = function () {
+            // For page 3/3
+            $scope.recoverPassword = function () {
                 $scope.validateConnection(recoverConnectedCallback, offlineCallback);
             };
 
             function recoverConnectedCallback() {
-                var errors = [];
-                var passwordPolicy = "La contraseña debe contener al menos 8 caracteres, incluir un caracter especial, una letra mayúscula, una minúscula y un número.";
-                var passwordsHaveValidFormat = false;
-                var passwordsCoincide = false;
 
-                if ($scope.recoverPasswordModel.password) {
-                    passwordsHaveValidFormat = true;
-                } else {
-                    errors.push(passwordPolicy);
-                }
+                $scope.$emit('ShowPreloader');
 
-                if (passwordsHaveValidFormat) {
-                    passwordsCoincide = checkEqualityOfPasswords($scope.recoverPasswordModel.password, $scope.recoverPasswordModel.confirmPassword);
-                }
+                $http({
+                    method: 'PUT',
+                    url: API_RESOURCE.format("authentication"),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: $.param({
+                        email: $scope.email,
+                        password: $scope.recoverPasswordModel.password,
+                        recoverycode: $scope.recoverPasswordModel.code
+                    })
+                }).success(function (data, status, headers, config) {
+                    //    Update the new username/password pair in Local Storage.
+                    var Credentials = JSON.parse(localStorage.getItem("Credentials"));
 
-                if (!passwordsCoincide && $scope.recoverPasswordModel.password) {
-                    errors.push("Las contraseñas capturadas no coinciden.");
-                }
+                    if (Credentials) {
+                        Credentials.password = $scope.recoverPasswordModel.password;
+                        Credentials.username = $scope.recoverPasswordModel.userOrEmail;
+                        localStorage.setItem("Credentials", JSON.stringify(Credentials));
+                    }
 
-                if (!$scope.recoverPasswordForm.code.$valid) {
-                    errors.push("Código requerido.");
-                }
-
-                $scope.recoverPasswordModel.modelState.errorMessages = errors;
-
-
-                if (errors.length === 0) {
-                    $scope.$emit('ShowPreloader');
-
-                    $http({
-                        method: 'PUT',
-                        url: API_RESOURCE.format("authentication"),
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                        data: $.param({
-                            email: $scope.recoverPasswordModel.email,
-                            password: $scope.recoverPasswordModel.password,
-                            recoverycode: $scope.recoverPasswordModel.code
-                        })
-                    }).success(function (data, status, headers, config) {
-
-                        $scope.$emit('HidePreloader');
-                        $scope.recoveredPassword = true;
-                        $scope.successMessage = "Se ha restablecido tu contraseña, ahora puedes iniciar sesión.";
-                        $scope.$emit('scrollTop');
-
-                        $scope.readOnly = true;
-
-                    }).error(function (data, status, headers, config) {
-
-                        $scope.$emit('HidePreloader');
-                        var errorMessage;
-                        if ((data != null && data.messageerror != null)) {
-                            errorMessage = window.atob(data.messageerror);
-                        } else {
-                            errorMessage = "Problema con la red; asegúrate de tener Internet e intenta de nuevo.";
-                        }
-
-                        $scope.recoverPasswordModel.modelState.errorMessages = [errorMessage];
-                        $scope.$emit('scrollTop');
-                    });
-                } else {
+                    $scope.recoveredPassword = true;
+                    $scope.successMessage = "Se ha restablecido su contraseña, ahora puedes iniciar sesión.";
+                    $scope.readOnly = true;
+                    $scope.recoverPasswordModel.modelState.isValid = true;
+                    //$scope.recoverPasswordModel.modelState.errorMessages = [];
+                    $scope.$emit('HidePreloader');
                     $scope.$emit('scrollTop');
-                }
+
+                }).error(function (data, status, headers, config) {
+
+                    var errorMessage;
+                    if ((data != null && data.messageerror != null)) {
+                        errorMessage = window.atob(data.messageerror);
+                    } else {
+                        errorMessage = "Problema con la red, asegúrate de tener Internet e intenta de nuevo.";
+                    }
+                    $scope.recoverPasswordModel.modelState.isValid = false;
+                    $scope.recoverPasswordModel.modelState.errorMessages = [errorMessage];
+                    $scope.$emit('HidePreloader');
+                    $scope.$emit('scrollTop');
+                });
+
             }
-        }]);
+
+            function offlineCallback() {
+                $timeout(function () {
+                    $scope.recoverPasswordModel.modelState.errorMessages = ["Se necesita estar conectado a Internet para continuar."];
+                    $scope.$emit('scrollTop');
+                }, 1000);
+            }
+
+
+
+
+        }]).controller('termsAndConditionsController', function ($scope, $modalInstance) {
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});;
