b92d082b86a729f02be739e12aba8352a286cfb4
U-SIEENA\antonio.monge <antonio.monge@DF-JAMP-01.sieenasoftware.com>
2016-03-08 12:10:20 -0600

    INC-4030, Spinner al hacer login con Facebook es tardado o no existe.

--- Scripts/app/public/controllers/login.js
+++ Scripts/app/public/controllers/login.js
@@ -279,6 +279,7 @@ angular
             }
 
             function FacebookLoginSuccess(data) {
+                $scope.$emit('ShowPreloader');
                 var userFacebook = JSON.parse(data);
 
                 _loadDrupalResources();
