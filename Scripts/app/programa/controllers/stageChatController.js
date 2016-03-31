7a50c359b69c09d7a582232ce187d3a1b7e3b630
Carlos Wilson <carlos.wilson@definityfirst.com>
2016-03-31 11:10:22 -0600

    INC-4136 Ajustar el texto CABINA DE SOPORTE en Cierre

--- Scripts/app/programa/controllers/stageChatController.js
+++ Scripts/app/programa/controllers/stageChatController.js
@@ -66,7 +66,7 @@ angular
                 var treeActivity = getActivityByActivity_identifier(activityIdentifier, userCourse);
                 $scope.aaa = treeActivity;
                 $scope.activityPoints = treeActivity.points;
-                $scope.activityname = treeActivity.sectionname;
+                $scope.activityname = treeActivity.sectionname[0] + treeActivity.sectionname.substring(1);
 
                 $scope.goChat = function () {
                     localStorage.setItem("chatRead/" + localStorage.getItem("userId"), "true");
